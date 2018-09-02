e6ee8445e71234ef9a21c6720d7857969b361812
Tim Urista <timothy.urista@gmail.com>
2018-09-02 11:38:07 -0700

    [IN-MODAL] update profile ui component

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -6,7 +6,7 @@ class Profile extends React.Component {
   state = {
     name: this.props.user.name || '',
     age: this.props.user.age || '',
-    pet: this.props.user.pet || '',
+    pet: this.props.user.pet || '',  
   }
 
   onFormChange = event => {
@@ -27,6 +27,7 @@ class Profile extends React.Component {
 
   render() {
     const { toggleModal, user } = this.props;
+    const { name, age, pet } = this.state;
     return (
       <div className="profile-modal">
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -34,30 +35,33 @@ class Profile extends React.Component {
             <img
               src="http://tachyons.io/img/logo.jpg"
               className="h3 w3 dib" alt="avatar" />
-            <h1>{user.name}</h1>
+            <h1>{name}</h1>
             <h4>{`Images Submitted: ${user.entries}`}</h4>
             <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
             <hr />
             <label className="mt2 fw6" htmlFor="username">Name:</label>
             <input
+              onChange={this.onFormChange}
               className="pa2 ba w-100"
-              placeholder={user.name}
+              placeholder={name}
               type="text"
               name="name"
               id="name"
             />
             <label className="mt2 fw6" htmlFor="age">Age:</label>
             <input
+              onChange={this.onFormChange}
               className="pa2 ba w-100"
-              placeholder={'56'}
+              placeholder={age}
               type="text"
               name="age"
               id="age"
             />
             <label className="mt2 fw6" htmlFor="pet">Pet:</label>
             <input
+              onChange={this.onFormChange}
               className="pa2 ba w-100"
-              placeholder={'dragon'}
+              placeholder={pet}
               type="text"
               name="pet"
               id="pet"
